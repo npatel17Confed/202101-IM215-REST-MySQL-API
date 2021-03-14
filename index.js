10905907027019649d1ba7b9de7442059e73cb23
Nilkumar Patel <nsp4898@gmail.com>
2021-03-14 16:49:49 -0400

    Step 3 - request and response using express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log("Request received");
+    // res.send('Welcome');
+    res.json({msg: "Welcome using JSON"});
+});
+
+app.listen(1234, () => {
+    console.log("Server is up and running on port 1234");
+});
