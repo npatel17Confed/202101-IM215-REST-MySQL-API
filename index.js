e34aa93225441d1a1d8fc794ef121f0e04616668
Nilkumar Patel <nsp4898@gmail.com>
2021-03-14 17:37:51 -0400

    Step 6 - morgan setup completed

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+app.use(morgan('combined'));
+// app.use(morgan('short'));
 
 app.get('/', (req, res) => {
     console.log("Request received");
     // res.send('Welcome');
-    // res.json({msg: "Welcome using JSON"});
+    res.json({msg: "Welcome using JSON"});
     res.end();
 });
 
